f50bdacdb37f6f4ecfaa942ca24e01ae429bdeed
Shreyas Grampurohit <shreyasg0512@gmail.com>
2022-11-16 18:37:33 +0530

    basic commit

---
+++ Code/ALU.vhdl
@@ -0,0 +1,28 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+-- The Arithmetic and the Logical Unit of IITB-CPU
+
+entity ALU is
+
+	port(
+    -- inputs
+        ALU_A,ALU_B:in std_logic_vector(2 downto 0);
+		c_in,z_in,clock:in std_logic;
+        ALU_J,ALU_CND:in std_logic_vector(1 downto 0);
+
+    -- outputs
+		ALU_C, ALU_Z, Z_int: out std_logic;
+        ALU_S: out std_logic_vector(2 downto 0));
+
+end ALU;
+
+
+architecture behavioural of ALU is
+begin
+
+-- works on the rising edge of the clock
+if(clock='1' and clock' event) then
+    
+end if;
