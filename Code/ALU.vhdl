ec287e6cce0234ceaa9d739508e90ea4b7b55a34
Shreyas Grampurohit <shreyasg0512@gmail.com>
2022-11-17 19:21:50 +0530

    designed it better

--- Code/ALU.vhdl
+++ Code/ALU.vhdl
@@ -1,12 +1,15 @@
+-- Preliminaries
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 -- The Arithmetic and the Logical Unit of IITB-CPU
+-- See ALU Documentation in the Report for details regarding inputs and outputs of ALU
 
 entity ALU is
 
 	port(
+
     -- inputs
         ALU_A,ALU_B:in std_logic_vector(2 downto 0);
 		c_in,z_in,clock:in std_logic;
@@ -14,15 +17,17 @@ entity ALU is
 
     -- outputs
 		ALU_C, ALU_Z, Z_int: out std_logic;
-        ALU_S: out std_logic_vector(2 downto 0));
+        ALU_S: out std_logic_vector(2 downto 0)
+        );
 
 end ALU;
-
 
 architecture behavioural of ALU is
 begin
 
--- works on the rising edge of the clock
-if(clock='1' and clock' event) then
-    
-end if;
+    -- works on the rising edge of the clock
+    if(clock='1' and clock' event) then
+        -- write code
+    end if;
+
+end architecture behavioural;
