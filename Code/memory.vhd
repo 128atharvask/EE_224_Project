672fc18e73bf76325970a91dc08eb6912ec0ae28
Atharva Kulkarni <210070047@iitb.ac.in>
2022-11-20 19:40:04 +0530

    memory and reg_file done

--- Code/memory.vhd
+++ Code/memory.vhd
@@ -4,39 +4,27 @@ use ieee.numeric_std.all;
 
 entity memory is
     port(
-        --clock
         clk : in std_logic;
-
-        --write enable bit
-        MWR: in std_logic;
-
-        --read enable bit
-        MDR: in std_logic;
-
-        --16 bit address
-        M_add : in std_logic_vector(15 downto 0);
-
-        --data input to the memory
-        M_data_in: in std_logic_vector(15 downto 0);
-
-        --data output from the memory
-        M_data_out: out std_logic_vector(15 downto 0));
-    
+        MWR : in std_logic; --write enable bit
+        MDR : in std_logic; --read enable bit
+        reset : in std_logic; --reset bit
+        M_add : in std_logic_vector(15 downto 0);--16 bit address
+        M_data_in : in std_logic_vector(15 downto 0); --data input to the memory
+        M_data_out : out std_logic_vector(15 downto 0));--data output from the memory
 end entity;
 
-architecture memory_a of memory is
-type memory_array is array( 0 to 65535 ) of std_logic_vector(15 downto 0);
-signal data : memory_array := (0 => "0000001000000000", 1 => "0010000111001001", others => (others => '1')); --Load with Program
-
+architecture memory_arch of memory is
+type mem_arr is array( 0 to 65535 ) of std_logic_vector(15 downto 0);
+signal data : mem_arr := (others => (others => '0'));
 begin
-    proc: process(clk)
+    mem_proc: process(MWR, MDR)
     begin
-
-        if (clk'event and clk = '0') then  
+        if (clk'event and clk = '0') then   --negative clock cycle  
             if (MWR = '1') then
                 data(to_integer(unsigned(M_add))) <= M_data_in;
             elsif(MDR = '1') then
-					M_data_out <= data(to_integer(unsigned(M_add)));
+                M_data_out <= data(to_integer(unsigned(M_add)));
+            end if;
         end if;
-    end process proc;
-end architecture memory_a;
+    end process mem_proc;
+end architecture memory_arch;
