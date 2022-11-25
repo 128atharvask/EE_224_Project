ed18d9b6c0dc54781fb5da1b97cb4781742e6482
Atharva Kulkarni <210070047@iitb.ac.in>
2022-11-25 21:19:15 +0530

    ALU basic TRACEFILE passed

--- Code/ALU.vhdl
+++ Code/ALU.vhdl
@@ -8,20 +8,20 @@ use ieee.numeric_std.all;
 
 entity ALU is
 
-    generic(
-        operand_width : integer:=16
-    );
+	 generic(
+		 operand_width : integer:=16
+	 );
 
-	port(
+    port(
 
     -- inputs
-        ALU_A, ALU_B:in std_logic_vector((operand_width - 1) downto 0);
+        ALU_A, ALU_B:in std_logic_vector((operand_width-1) downto 0);
 		clock, c_in, z_in:in std_logic;
         ALU_J, ALU_CND:in std_logic_vector(1 downto 0);
 
     -- outputs
 		ALU_C, ALU_Z, Z_int: out std_logic;
-        ALU_S: out std_logic_vector((operand_width - 1) downto 0)
+        ALU_S: out std_logic_vector((operand_width-1) downto 0)
         );
 
 end ALU;
@@ -29,14 +29,14 @@ end ALU;
 architecture behavioural of ALU is
 
     -- Ripple Adder Implementation
-    function add(A: in std_logic_vector((operand_width-1) downto 0);
-                 B: in std_logic_vector(15 downto 0))
+    function add1(A: in std_logic_vector((operand_width-1) downto 0);
+                 B: in std_logic_vector((operand_width-1) downto 0))
     
         return std_logic_vector is
-            variable sum: std_logic_vector((operand_width-1) downto 0);
-            variable carry: std_logic_vector((operand_width-1) downto 0);
+            variable sum: std_logic_vector((operand_width-1) downto 0):= (others => '1');
+            variable carry: std_logic_vector((operand_width-1) downto 0) := (others => '0');
     begin
-        L1: for i in 0 to (operand_width-1) loop
+          L1: for i in 0 to (operand_width-1) loop
                 if i = 0 then 
                     sum(i) := A(i) xor B(i);
                     carry(i) := A(i) and B(i);
@@ -46,7 +46,7 @@ architecture behavioural of ALU is
                 end if;
             end loop L1;
         return carry((operand_width-1)) & sum;
-    end add;
+    end add1;
 
     -- Bitwise NAND implementation
     function nander(A: in std_logic_vector((operand_width-1) downto 0);
@@ -59,30 +59,32 @@ architecture behavioural of ALU is
 			end loop L2;
 		return bitwise_nand;
 	end nander;
-    
-    signal sum: std_logic_vector((operand_width-1) downto 0);
-    signal full_add: std_logic_vector((operand_width) downto 0);
-    signal carry: std_logic;
-    signal bitwise_nand: std_logic_vector((operand_width-1) downto 0);
-	signal sz_int: std_logic; -- Signal for temporarily storing Z_int
+	
 begin
 
     -- works on the rising edge of the clock
-    clock_proc:process(clock,ALU_J, ALU_CND, ALU_A, ALU_B, c_in, z_in)
-    begin
-        if(clock='1' and clock' event) then
+	 clock_proc:process(clock,ALU_J, ALU_CND, ALU_A, ALU_B, c_in, z_in)
+    variable sum: std_logic_vector((operand_width-1) downto 0) := "0000000000000001";
+    variable full_add: std_logic_vector((operand_width) downto 0) := "00000000000000000";
+    variable carry: std_logic := '0';
+    variable bitwise_nand: std_logic_vector((operand_width-1) downto 0) := "0000000000000000";
+	variable sz_int: std_logic := '0'; -- Signal for temporarily storing Z_int
+	 begin
+		  if(clock='1' and clock'event) then
 
             -- Case Addition
             if(ALU_J = "00") then
-                full_add <= add(ALU_A, ALU_B);
-                carry <= full_add((operand_width-1));
-                sum <= full_add((operand_width-1) downto 0);
+					
+                full_add := add1(ALU_A, ALU_B);
+                carry := full_add((operand_width));
+                sum := full_add((operand_width-1) downto 0);
                 ALU_S <= sum;
                 if(sum = "0000000000000000") then
-                    sz_int <= '1';
+                    sz_int := '1';
                 else
-                    sz_int <= '0';
+                    sz_int := '0';
                 end if;
+
                 if(ALU_CND = "00") then
                     ALU_C <= carry;
                     ALU_Z <= sz_int;
@@ -99,12 +101,12 @@ begin
             
             -- Case NAND
             elsif(ALU_J = "01") then -- NAND
-                bitwise_nand <= nander(ALU_A, ALU_B);
+                bitwise_nand := nander(ALU_A, ALU_B);
                 ALU_S <= bitwise_nand;
                 if(bitwise_nand = "0000000000000000") then
-                    sz_int <= '1';
+                    sz_int := '1';
                 else
-                    sz_int <= '0';
+                    sz_int := '0';
                 end if;
                 ALU_C <= c_in;
                 if(ALU_CND = "00") then
@@ -120,15 +122,15 @@ begin
             -- For BEQ
             elsif(ALU_J = "11") then
                 if(ALU_A = ALU_B) then
-                    sz_int <= '1';
+                    sz_int := '1';
                 else
-                    sz_int <= '0';
+                    sz_int := '0';
                 end if;
                 ALU_C <= c_in;
                 ALU_Z <= z_in;
             else null;
             end if;
-		Z_int <= sz_int;
+		      Z_int <= sz_int;
         end if;
     end process;
 end architecture behavioural;
@@ -155,4 +157,4 @@ end architecture behavioural;
 --           11: S1, S7, S9, S12, S17, S18, S20, S21
 --           T1_1-0: S3, 
 
--- Remember: Modify S12 and S14 - Done
+-- Remember: Modify S12 and S14 - Done
