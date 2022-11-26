164325019a58886f96761c2031fddd5f1ac8e283
Shreyas Grampurohit <shreyasg0512@gmail.com>
2022-11-26 13:41:47 +0530

    Reduced States

--- Code/DataTypePackage.vhdl
+++ Code/DataTypePackage.vhdl
@@ -8,5 +8,5 @@ package DataTypePackage is
 	type size2x16 is array(0 to 1) of std_logic_vector(15 downto 0);
 	type size2x3 is array(0 to 1) of std_logic_vector(2 downto 0);
 	type state is (s0,s1,s2,s3,s4,s5,s6,s7,s8,s9,
-		s10,s11,s12,s13,s14,s15,s16,s17,s18,s20);
+		s10,s11,s12,s13,s14,s15,s16,s17,s18,s19);
 end package;
